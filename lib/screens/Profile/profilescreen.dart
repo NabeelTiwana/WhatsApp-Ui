ae1daaa1544770d8a6df4806ab71268c101ea1b7
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-03-03 13:13:30 +0500

    proflie scrrrn completed

--- lib/screens/Profile/profilescreen.dart
+++ lib/screens/Profile/profilescreen.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:whatsapp_clone/widgets/uihelper.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
   TextEditingController nameController = TextEditingController();
+
+  File? pickedimage;
 
   @override
   Widget build(BuildContext context) {
@@ -24,13 +34,21 @@ class ProfileScreen extends StatelessWidget {
             ),
             UiHelper.customText(text: 'profile photo', height: 15),
             SizedBox(height: 30),
-            CircleAvatar(
-              radius: 70,
-              backgroundColor: Color(0XFFD9D9D9),
-              child: Image.asset(
-                'assets/images/camera.png',
-                height: 50,
-                fit: BoxFit.cover,
+            GestureDetector(
+              onTap: () {
+                _openButtom(context);
+              },
+              child: pickedimage==null? CircleAvatar(
+                radius: 70,
+                backgroundColor: Color(0XFFD9D9D9),
+                child: Image.asset(
+                  'assets/images/camera.png',
+                  height: 50,
+                  fit: BoxFit.cover,
+                ),
+              ):CircleAvatar(
+                radius: 70,
+                backgroundImage: FileImage(pickedimage!),
               ),
             ),
             SizedBox(height: 40),
@@ -53,14 +71,71 @@ class ProfileScreen extends StatelessWidget {
                     ),
                   ),
                 ),
+                SizedBox(width: 10),
+                Image.asset('assets/images/happyface .png'),
               ],
             ),
-           Image.asset('assets/images/happyface .png')
           ],
         ),
       ),
-      floatingActionButton: UiHelper.customButton(onPress: (){}, buttonname: 'Next'),
+      floatingActionButton: UiHelper.customButton(
+        onPress: () {},
+        buttonname: 'Next',
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
+  }
+
+  _openButtom(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          width: 200,
+          height: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      _pickImage(ImageSource.camera);
+                    },
+                    icon: Icon(Icons.camera_alt, size: 80, color: Colors.grey),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      _pickImage(ImageSource.gallery);
+                    },
+                    icon: Icon(Icons.image, size: 80, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  _pickImage(ImageSource imagesource) async {
+    try {
+      Navigator.pop(context);
+      final photo = await ImagePicker().pickImage(source: imagesource);
+      if (photo == null) return;
+      final tempImage = File(photo.path);
+      setState(() {
+        pickedimage = tempImage;
+      });
+    } catch (ex) {
+      return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(ex.toString()),
+          backgroundColor: Color(0XFF00A884),
+        ),
+      );
+    }
   }
 }
