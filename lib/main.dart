7fa38f284c9eafc0593ce16c4a27635b502ca299
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-03-08 01:12:29 +0500

    conect firebas

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/screens/home/homescreen.dart';
 import 'package:whatsapp_clone/screens/splash/splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
